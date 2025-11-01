096e6301ab028f8cfe8e7b0ea1871f74d585afbb
Ahmed Mostafa <ahmed.code.dev@gmail.com>
2025-11-01 13:04:42 +0200

    finished

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -19,3 +19,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
